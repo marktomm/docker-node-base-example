238b1d9683be18f3215c2eacbe0949f646219798
Mark Tomm <mark.tomm@martem.ee>
2024-05-02 16:09:31 +0300

    lesson 01.05.2024

--- server_02.js
+++ server_02.js
@@ -1,6 +1,16 @@
 const { createServer } = require('node:http');
 const fs = require('fs');
 const { argv } = require('node:process');
+const { Client } = require('pg');
+
+const dbconfig = {
+  host: 'localhost',
+  port: 5432,
+  user: 'project_docker_node',
+  password: 'project_docker_node',
+  database: 'project_docker_node',
+};
+const dbclient = new Client(dbconfig);
 
 const args = argv.slice(2);
 
@@ -20,7 +30,7 @@ if (indexPathIndex != -1) {
   indexPath = args[indexPathIndex + 1];
 }
 
-const serverFunction = function (request, response) {
+const serverFunction = async function (request, response) {
   console.log('method: ', request.method, ' url: ', request.url);
 
   if (request.method === 'GET') {
@@ -34,6 +44,11 @@ const serverFunction = function (request, response) {
       const imageData = fs.readFileSync(faviconPath);
       response.writeHead(200, { 'Content-Type': 'image/x-icon' });
       response.write(imageData);
+    } else if (request.url.startsWith('/adduser')) {
+      const name = 'new_user';
+      await dbclient.query('INSERT INTO users(name) VALUES($1)', [name]);
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.write('User ' + name + ' created');
     } else {
       response.statusCode = 404;
     }
@@ -46,8 +61,17 @@ const serverFunction = function (request, response) {
 
 const server = createServer(serverFunction);
 
-const serverReady = function () {
+const serverReady = async function () {
   console.log('listening on port ' + portNumber);
+
+  await dbclient.connect();
+
+  await dbclient.query(`
+  CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100)
+  );
+  `);
 };
 
 server.listen(portNumber, serverReady);
